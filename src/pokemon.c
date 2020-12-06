3908a7f00c2f832fba1339660fa8656020f35267
LOuroboros <lunosouroboros@gmail.com>
2020-12-06 19:00:31 -0300

    Disabled in battle Pokémon animations Thanks to GriffinR's instructions.

--- src/pokemon.c
+++ src/pokemon.c
@@ -6618,10 +6618,7 @@ static void Task_PokemonSummaryAnimateAfterDelay(u8 taskId)
 
 void BattleAnimateFrontSprite(struct Sprite* sprite, u16 species, bool8 noCry, u8 arg3)
 {
-    if (gHitMarker & HITMARKER_NO_ANIMATIONS && !(gBattleTypeFlags & (BATTLE_TYPE_LINK | BATTLE_TYPE_x2000000)))
-        DoMonFrontSpriteAnimation(sprite, species, noCry, arg3 | 0x80);
-    else
-        DoMonFrontSpriteAnimation(sprite, species, noCry, arg3);
+    DoMonFrontSpriteAnimation(sprite, species, noCry, arg3 | 0x80);
 }
 
 void DoMonFrontSpriteAnimation(struct Sprite* sprite, u16 species, bool8 noCry, u8 arg3)
@@ -6670,21 +6667,7 @@ void DoMonFrontSpriteAnimation(struct Sprite* sprite, u16 species, bool8 noCry, 
 
 void PokemonSummaryDoMonAnimation(struct Sprite* sprite, u16 species, bool8 oneFrame)
 {
-    if (!oneFrame && HasTwoFramesAnimation(species))
-        StartSpriteAnim(sprite, 1);
-    if (sMonAnimationDelayTable[species - 1] != 0)
-    {
-        u8 taskId = CreateTask(Task_PokemonSummaryAnimateAfterDelay, 0);
-        STORE_PTR_IN_TASK(sprite, taskId, 0);
-        gTasks[taskId].data[2] = sMonFrontAnimIdsTable[species - 1];
-        gTasks[taskId].data[3] = sMonAnimationDelayTable[species - 1];
-        SummaryScreen_SetUnknownTaskId(taskId);
-        SetSpriteCB_MonAnimDummy(sprite);
-    }
-    else
-    {
-        StartMonSummaryAnimation(sprite, sMonFrontAnimIdsTable[species - 1]);
-    }
+    sprite->callback = SpriteCallbackDummy;
 }
 
 void StopPokemonAnimationDelayTask(void)
@@ -6696,15 +6679,7 @@ void StopPokemonAnimationDelayTask(void)
 
 void BattleAnimateBackSprite(struct Sprite* sprite, u16 species)
 {
-    if (gHitMarker & HITMARKER_NO_ANIMATIONS && !(gBattleTypeFlags & (BATTLE_TYPE_LINK | BATTLE_TYPE_x2000000)))
-    {
-        sprite->callback = SpriteCallbackDummy;
-    }
-    else
-    {
-        LaunchAnimationTaskForBackSprite(sprite, GetSpeciesBackAnimSet(species));
-        sprite->callback = SpriteCallbackDummy_2;
-    }
+    sprite->callback = SpriteCallbackDummy;
 }
 
 u8 sub_806EF08(u8 arg0)
