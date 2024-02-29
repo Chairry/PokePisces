51c2eb665ba5da87cf4cbf044d8c35a16a723c5f
Chairry <chairvaria@gmail.com>
2024-02-29 12:43:51 -0500

    new WOODEN battle bg, started flotshore events

--- src/battle_anim_utility_funcs.c
+++ src/battle_anim_utility_funcs.c
@@ -144,6 +144,9 @@ void AnimTask_SetCamouflageBlend(u8 taskId)
     case BATTLE_TERRAIN_GOLD_ROUTE:
         gBattleAnimArgs[4] = RGB(12, 24, 2);
         break;
+    case BATTLE_TERRAIN_WOODEN_ROUTE:
+        gBattleAnimArgs[4] = RGB(12, 24, 2);
+        break;
     case BATTLE_TERRAIN_BUILDING:
         gBattleAnimArgs[4] = RGB_WHITE;
         break;
