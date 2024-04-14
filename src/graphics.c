465e3ecc2ff41d6cfe764f7dd20146eed8039231
mudskipper13 <MariaJumpMan001@hotmail.com>
2024-04-14 14:48:55 +0700

    Shop UI: Rename mugshots folder to sellers for later

--- src/graphics.c
+++ src/graphics.c
@@ -1682,17 +1682,17 @@ const u32 gShopMenu_ScrollTilemap[] = INCBIN_U32("graphics/shop/scroll.bin.lz");
 const u32 gShopMenuMoney_Gfx[] = INCBIN_U32("graphics/shop/money.4bpp.lz");
 const u16 gShopMenu_CursorGfx[] = INCBIN_U16("graphics/shop/cursor.4bpp"); // uses the menu palette
 
-const u8 gShopMenuSellerMugshotGfx_Jerry[] = INCBIN_U8("graphics/shop/mugshots/jerry.4bpp");
-const u16 gShopMenuSellerMugshotPal_Jerry[] = INCBIN_U16("graphics/shop/mugshots/jerry.gbapal");
+const u8 gShopMenuSellerMugshotGfx_Jerry[] = INCBIN_U8("graphics/shop/sellers/jerry.4bpp");
+const u16 gShopMenuSellerMugshotPal_Jerry[] = INCBIN_U16("graphics/shop/sellers/jerry.gbapal");
 
-const u8 gShopMenuSellerMugshotGfx_Jennie[] = INCBIN_U8("graphics/shop/mugshots/jennie.4bpp");
-const u16 gShopMenuSellerMugshotPal_Jennie[] = INCBIN_U16("graphics/shop/mugshots/jennie.gbapal");
+const u8 gShopMenuSellerMugshotGfx_Jennie[] = INCBIN_U8("graphics/shop/sellers/jennie.4bpp");
+const u16 gShopMenuSellerMugshotPal_Jennie[] = INCBIN_U16("graphics/shop/sellers/jennie.gbapal");
 
-const u8 gShopMenuSellerMugshotGfx_Purplina[] = INCBIN_U8("graphics/shop/mugshots/purplina.4bpp");
-const u16 gShopMenuSellerMugshotPal_Purplina[] = INCBIN_U16("graphics/shop/mugshots/purplina.gbapal");
+const u8 gShopMenuSellerMugshotGfx_Purplina[] = INCBIN_U8("graphics/shop/sellers/purplina.4bpp");
+const u16 gShopMenuSellerMugshotPal_Purplina[] = INCBIN_U16("graphics/shop/sellers/purplina.gbapal");
 
-const u8 gShopMenuSellerMugshotGfx_Kodough[] = INCBIN_U8("graphics/shop/mugshots/kodough.4bpp");
-const u16 gShopMenuSellerMugshotPal_Kodough[] = INCBIN_U16("graphics/shop/mugshots/kodough.gbapal");
+const u8 gShopMenuSellerMugshotGfx_Kodough[] = INCBIN_U8("graphics/shop/sellers/kodough.4bpp");
+const u16 gShopMenuSellerMugshotPal_Kodough[] = INCBIN_U16("graphics/shop/sellers/kodough.gbapal");
 
 // Pokeblock
 
