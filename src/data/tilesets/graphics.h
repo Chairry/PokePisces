8e99a2e0565966f8b4037eee842669dbbf1eaf26
Chairry <chairvaria@gmail.com>
2024-04-18 13:47:03 -0400

    began work on ozone lilycove hq

--- src/data/tilesets/graphics.h
+++ src/data/tilesets/graphics.h
@@ -1805,3 +1805,22 @@ const u16 gTilesetPalettes_BirchLab[][16] =
 };
 
 const u32 gTilesetTiles_BirchLab[] = INCBIN_U32("data/tilesets/secondary/birch_lab/tiles.4bpp.lz");
+
+const u16 gTilesetPalettes_OzoneInterior[][16] =
+{
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/00.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/01.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/02.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/03.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/04.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/05.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/06.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/07.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/08.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/09.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/10.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/11.gbapal"),
+    INCBIN_U16("data/tilesets/secondary/ozone_interior/palettes/12.gbapal"),
+};
+
+const u32 gTilesetTiles_OzoneInterior[] = INCBIN_U32("data/tilesets/secondary/ozone_interior/tiles.4bpp.lz");
