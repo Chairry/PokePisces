d9dd430c2a2a2a915ff26c240befe90153461018
mudskipper13 <MariaJumpMan001@hotmail.com>
2024-06-13 20:31:59 +0700

    Misc.: Add a small formatting function, credit to Vexx on pret

--- src/international_string_util.c
+++ src/international_string_util.c
@@ -234,3 +234,49 @@ void FillWindowTilesByRow(int windowId, int columnStart, int rowStart, int numFi
         }
     }
 }
+
+// credit to Vexx on PRET Discord
+void FormatTextByWidth(u8 *result, s32 maxWidth, u8 fontId, const u8 *str, s16 letterSpacing)
+{
+    u8 *end, *ptr, *curLine, *lastSpace;
+
+    end = result;
+    // copy string, replacing all space and line breaks with EOS
+    while (*str != EOS) 
+    {
+        if (*str == CHAR_SPACE || *str == CHAR_NEWLINE) 
+            *end = EOS;
+        else 
+            *end = *str;
+
+        end++;
+        str++;
+    }
+    *end = EOS; // now end points to the true end of the string
+
+    ptr = result;
+    curLine = ptr;
+
+    while (*ptr != EOS)
+        ptr++;
+    // now ptr is the first EOS char
+
+    while (ptr != end) 
+    {
+        // all the EOS chars (except *end) must be replaced by either ' ' or '\n'
+        lastSpace = ptr++; // this points at the EOS
+
+        // check that adding the next word this line still fits
+        *lastSpace = CHAR_SPACE;
+        if (GetStringWidth(fontId, curLine, letterSpacing) > maxWidth) 
+        {
+            *lastSpace = CHAR_NEWLINE;
+
+            curLine = ptr;
+        }
+
+        while (*ptr != EOS) 
+            ptr++;
+        // now ptr is the next EOS char
+    }
+}
